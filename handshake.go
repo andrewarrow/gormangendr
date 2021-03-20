afd0cd0faa4bc90ab5184b3fb6e6e4b3b30b1e03
Andrew Arrow <oneone@gmail.com>
2021-03-20 15:38:09 -0700

    clean

--- handshake.go
+++ handshake.go
@@ -43,44 +43,37 @@ func Handshake(host string) {
 		fmt.Println("dial error:", err)
 		return
 	}
-	fmt.Println("trying2", host)
 	defer conn.Close()
 
 	queryNode(conn, multiplex.MiniProtocolIDMuxControl, handshakeRequest())
 
 	chainSyncRequest := cbor.NewArray()
 	chainSyncRequest.Add(cbor.NewPositiveInteger(0))
-	queryNode(conn, multiplex.MiniProtocolIDChainSyncBlocks, []cbor.DataItem{chainSyncRequest})
+	messageResponse := queryNode(conn, multiplex.MiniProtocolIDChainSyncBlocks, []cbor.DataItem{chainSyncRequest})
+	fmt.Println(messageResponse.Debug())
 }
 
-func queryNode(conn net.Conn, miniProtocol multiplex.MiniProtocol, dataItems []cbor.DataItem) {
+func queryNode(conn net.Conn, miniProtocol multiplex.MiniProtocol, dataItems []cbor.DataItem) *multiplex.ServiceDataUnit {
 	sdu := multiplex.NewServiceDataUnit(miniProtocol, multiplex.MessageModeInitiator, dataItems)
-	fmt.Println(sdu.Bytes())
 	conn.Write(sdu.Bytes())
 	response := []byte{}
 	for {
-		fmt.Println("h1", len(response))
 		bytesHeader := make([]byte, multiplex.HeaderSize)
 		conn.Read(bytesHeader)
-		fmt.Println("header", bytesHeader, len(bytesHeader))
-		header, err := multiplex.ParseHeader(bytesHeader)
-		fmt.Println("header", header, err)
+		header, _ := multiplex.ParseHeader(bytesHeader)
 
 		response = append(response, header.Bytes()...)
 		totalReadBytes := 0
 		for totalReadBytes < header.PayloadLengthAsInt32() {
 			buf := make([]byte, header.PayloadLengthAsInt32()-totalReadBytes)
-			fmt.Println("buf", len(buf), header.PayloadLengthAsInt32())
 			readBytes, _ := conn.Read(buf)
 			response = append(response, buf[:readBytes]...)
 			totalReadBytes += readBytes
 		}
-		fmt.Println("buf2", header.PayloadLength(), multiplex.MaxSDUSize)
 		if int(header.PayloadLength()) != multiplex.MaxSDUSize {
 			break
 		}
 	}
 	sdus, _ := multiplex.ParseServiceDataUnits(response)
-	fmt.Println(sdus[0].Debug())
-	fmt.Println("r", len(sdus), response)
+	return sdus[0]
 }
